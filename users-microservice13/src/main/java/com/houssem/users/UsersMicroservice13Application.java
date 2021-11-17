0569554c0010b4906facada7ce13dfc7cd0d426b
houssemhouss <84544211+houssemhouss@users.noreply.github.com>
2021-11-18 00:38:08 +0100

    Add files via upload

---
+++ users-microservice13/src/main/java/com/houssem/users/UsersMicroservice13Application.java
@@ -0,0 +1,51 @@
+
+package com.houssem.users;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.houssem.users.entities.Role;
+import com.houssem.users.entities.User;
+import com.houssem.users.service.UserService;
+
+@SpringBootApplication
+public class UsersMicroservice13Application {
+	@Autowired
+	private UserService userService;
+	public static void main(String[] args) {
+		SpringApplication.run(UsersMicroservice13Application.class, args);
+	}
+
+	/*@PostConstruct
+	void init_users() {
+	
+	userService.addRole(new Role(null,"ADMIN"));
+	userService.addRole(new Role(null,"USER"));
+	
+	//ajouter les users
+	userService.saveUser(new User(null,"Wissal","246",true,null));
+	userService.saveUser(new User(null,"Ahmed","123",true,null));
+	userService.saveUser(new User(null,"Yassine","123",true,null));
+	
+	//ajouter les rôles aux users
+	userService.addRoleToUser("Wissal", "ADMIN");
+	userService.addRoleToUser("Yassine", "USER");
+	userService.addRoleToUser("Ahmed", "USER");
+	userService.addRoleToUser("Yassine", "USER");
+	} */
+	@Bean
+	BCryptPasswordEncoder getBCE() {
+	return new BCryptPasswordEncoder();
+	}
+
+	
+	
+
+
+
+}
---
+++ users-microservice13/src/main/java/com/houssem/users/service/UserService.java
@@ -0,0 +1,14 @@
+package com.houssem.users.service;
+
+import org.springframework.stereotype.Service;
+
+import com.houssem.users.entities.Role;
+import com.houssem.users.entities.User;
+
+
+public interface UserService {
+	User saveUser(User user);
+	User findUserByUsername (String username);
+	Role addRole(Role role);
+	User addRoleToUser(String username, String rolename);
+}
